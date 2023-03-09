c8522e6aca2321ef511c28fa38e83f23e1ba814a
Ashif Uzzaman <a.uzzaman1206@gmail.com>
2023-03-09 17:20:10 -0500

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:'https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407'
+    baseUrl:'https://fs2.formsite.com/meherpavan/form2/',
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      charts: true,
+      reportPageTitle: "My Test Suite",
+      embeddedScreenshots: true,
+      inlineAssets: true
+    },
+  video: false,
+    
   },
 });
--- cypress/e2e/new_contact_form.cy.js
+++ cypress/e2e/new_contact_form.cy.js
@@ -1,14 +1,33 @@
 /// <reference types="cypress" />
-const testData= require("../fixtures/example.json")
+const testData= require("../fixtures/basicTestData.json")
 
 describe("All test cases related to new contact page", ()=>{
 
     testData.forEach((element) => {
-        it('Test invalid inputs', ()=>{
-            cy.visit('/');
-            cy.findByLabelText("First Name")
-            .type(element.FirstName);
-        })
+        if(element.TestName.includes('happy')){
+            it(element.TestName, ()=>{
+                cy.visit('/');
+                cy.findByLabelText("First Name").type(element.firstName);
+                cy.findByLabelText("Last Name").type(element.lastName);
+                cy.findByLabelText("Phone").type(element.mPhone);
+                cy.findByLabelText("Email Address").type(element.pEmail);
+            })
+        }else if(element.TestName.includes('required')){
+            it(element.TestName, ()=>{
+                cy.visit('/');
+                cy.findByLabelText("Last Name").type(element.lastName);
+
+            })
+        }else {
+            it(element.TestName, ()=>{
+                cy.visit('/');
+                cy.findByLabelText("First Name").type(element.firstName);
+                cy.findByLabelText("Last Name").type(element.lastName);
+                cy.findByLabelText("Phone").type(element.mPhone);
+                cy.findByLabelText("Email Address").type(element.pEmail);
+            })
+        }
+
 
     });
 })
