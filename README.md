b2764fce59498fd58a9960df7abf2cf38c1792c2
Ashif Uzzaman <a.uzzaman1206@gmail.com>
2023-03-09 17:18:15 -0500

    Updated custom command and reports

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MoveHQ_skill_Assesment
 This is the skill assessment for MoveHQ SDET Lead position.
+## Description
+This is an UI atomation using cypress.
+
+## Assumptions
+Here are some assumption made while creating the automation script:-
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,20 +26,56 @@ import '@testing-library/cypress/add-commands';
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('fillNewContactForm',(testType,contactType,customerNumber,firstName,lastName,oPhone,mPhone,hPhone,pEmail,sEmail)=>{
-    cy.findByLabelText('Contact Type').select(contactType);
-    cy.findByLabelText('Customer Number').type(customerNumber);
-    cy.findByLabelText('First Name').type(firstName);
-    cy.findByLabelText('Last Name').type(lastName);
-    cy.findByLabelText('Office Phone').type(oPhone);
-    cy.findByLabelText('Mobile Phone').type(mPhone);
-    cy.findByLabelText('Home Phone').type(hPhone);
-    cy.findByLabelText('Primary Email').type(pEmail);
-    cy.findByLabelText('Secondary Email').type(sEmail);
-    cy.findByText("Save").click();
-    if(testType=='Invalid Characters'){
-        cy.findAllByRole('alert').should()
+Cypress.Commands.add('fillNewContactFormWithAllFields',(contactType,customerNumber,firstName,lastName,oPhone,mPhone,hPhone,pEmail,sEmail)=>{
+    if(contactType!=null){
+        cy.findByLabelText('Contact Type').select(contactType);
     }
-    
+    if(customerNumber!=null){
+        cy.findByLabelText('Customer Number').type(customerNumber);
+    }
+    if(firstName!=null){
+        cy.findByLabelText('First Name').type(firstName);
+    }
+    if(lastName!=null){
+        cy.findByLabelText('Last Name').type(lastName);
+    }
+    if(oPhone!=null){
+        cy.findByLabelText('Office Phone').type(oPhone);
+    }
+    if(mPhone!=null){
+        cy.findByLabelText('Mobile Phone').type(mPhone);
+    }
+    if(hPhone!=null){
+        cy.findByLabelText('Home Phone').type(hPhone);
+    }
+    if(pEmail!=null){
+        cy.findByLabelText('Primary Email').type(pEmail);
+    }
+    if(sEmail!=null){
+        cy.findByLabelText('Secondary Email').type(sEmail);
+    }
+   
+    cy.findByText("Save").click();
 
+})
+
+Cypress.Commands.add("validateErrorsNewContactForms",(testType)=>{
+    const allAlertMessages= cy.findAllByRole('alert');
+    if(testType.includes('Invalid Characters')){
+        expect(allAlertMessages).includes("Name field only accepts alphabet charactors"|
+        "Phone number can only accept numbers and the following characters: -,),(,+"|
+        "Email needs to be a valid email address"|
+        "Customer number can only be integer values")
+    }
+    if(testType.includes('missing required fields')){
+        expect(allAlertMessages).includes("Last Name Field cannot be empty"|
+        "Contact type field cannot be empty"|
+        "Email needs to be a valid email address"|
+        "Assigned to field cannot be empty"|
+        "Agent owner cannot be empty")
+    }
+})
+
+Cypress.Commands.add("validateNewContactCreated",(testData)=>{
+    
 })
